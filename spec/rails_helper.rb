8ac0582798d3873bf5e0dd3c679cf678554c30b0
iavianm <iavianm@mail.ru>
2023-10-16 16:46:25 +0300

    add rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,13 +5,13 @@ require 'vcr'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  config.hook_into :webmock
+  config.hook_into(:webmock)
   config.configure_rspec_metadata!
 
   # config.filter_sensitive_data('<ACC_WEATHER_API_KEY>') { ENV['ACC_WEATHER_API_KEY'] }
@@ -37,7 +37,7 @@ end
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
-  abort e.to_s.strip
+  abort(e.to_s.strip)
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures

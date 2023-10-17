5c734c30b6d593564fc781b9a29f5f0cd0e0e040
iavianm <iavianm@mail.ru>
2023-10-17 16:30:54 +0300

    add validations

--- app/models/weather_record.rb
+++ app/models/weather_record.rb
@@ -1,2 +1,13 @@
 class WeatherRecord < ApplicationRecord
+  validates :city, :data, presence: true
+  validates :city, length: { minimum: 2, maximum: 50 }
+  validate :data_is_valid_json
+
+  private
+
+  def data_is_valid_json
+    JSON.parse(data)
+  rescue JSON::ParserError => e
+    errors.add(:data, "is not a valid JSON: #{e.message}")
+  end
 end
